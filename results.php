45f67fd85dd9e394bfaaeaa5c7bce72425815332
Nouster <djebali.mohamed88@gmail.com>
2023-03-02 08:44:10 +0100

    insensitive case research and dynamic spelling for search results.

--- results.php
+++ results.php
@@ -1,14 +1,16 @@
 <?php require_once 'data/library.php';
 ['q' => $search] = $_GET;
-
+$search = strtolower($search);
 // var_dump($_GET);
 
-$results = array_filter($libraryGames, fn ($game)=> str_contains($game['gameName'], $search));
+// Conversion en miniscule pour rendre la recherche insensible à la casse
+$results = array_filter($libraryGames, fn ($game)=> str_contains(strtolower($game['gameName']), $search));
 
 require_once 'layout/header.php';?>
 
 <h2>Résultats de la recherche pour "<?php echo $search; ?>" :</h2>
-<h3><?php echo count($results) ?> résultat trouvé</h3>
+<!-- Rendre l'orthographe dynamique -->
+<h3><?php echo count($results) ?> résultat<?php echo count($results) > 1 ? "s":"" ?> trouvé<?php echo count($results) > 1 ? "s":""?></h3>
 
 
 <section class="container">
